4590e7e93c0a8d97f37865f3113e1acc538d15b8
sheikhasiad-creator <sheikhasiad@gmail.com>
2026-01-04 00:03:00 +0400

    Create algorithm.py

---
+++ algorithm.py
@@ -0,0 +1,19 @@
+# Algorithm to identify best-selling product
+ 
+sales = [100, 200, 150, 300, 250]
+
+best_sale = 0
+ 
+for sale in sales:        # loop
+
+    if sale > best_sale:  # if condition
+
+        best_sale = sale
+
+    else:
+
+        best_sale = best_sale
+ 
+print("Best selling value:", best_sale)
+
+ 
